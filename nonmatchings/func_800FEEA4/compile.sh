7c76cd6b1627318980501841fe817cce6f18ef1f
Joey Murphy <tjmurphy@mit.edu>
2023-10-01 10:42:45 -0400

    delete temp files

--- nonmatchings/func_800FEEA4/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-INPUT="$(realpath "$1")"
-OUTPUT="$(realpath "$3")"
-cd /home/joey/git_repos/sotn-decomp
-mipsel-linux-gnu-cpp -Iinclude -undef -Wall -fno-builtin -Dmips -D__GNUC__=2 -D__OPTIMIZE__ -D__mips__ -D__mips -Dpsx -D__psx__ -D__psx -D_PSYQ -D__EXTENSIONS__ -D_MIPSEL -D_LANGUAGE_C -DLANGUAGE_C -DHACKS -DUSE_INCLUDE_ASM -D_internal_version_us -lang-c "$INPUT" | iconv --from-code=UTF-8 --to-code=Shift-JIS | ./bin/cc1-psx-26 -G0 -w -O2 -funsigned-char -fpeephole -ffunction-cse -fpcc-struct-return -fcommon -fverbose-asm -msoft-float -g -quiet -mcpu=3000 -fgnu-linker -mgas -gcoff | python3 tools/maspsx/maspsx.py --no-macro-inc --expand-div | mipsel-linux-gnu-as -Iinclude -march=r3000 -mtune=r3000 -no-pad-sections -O1 -G0 -o "$OUTPUT"
