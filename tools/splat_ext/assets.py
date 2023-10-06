892f368d2fea26293c183b335487e8d1ea97b359
Joey Murphy <tjmurphy@mit.edu>
2023-10-06 15:36:57 -0400

    Working extraction of equipment and accessory

--- tools/splat_ext/assets.py
+++ tools/splat_ext/assets.py
@@ -12,16 +12,22 @@ from util import options, log
 from segtypes.n64.segment import N64Segment
 import utils
 
-item_size = 0x34  # sizeof(Equipment)
+#item_size = 0x34  # sizeof(Equipment)
 
 def get_serializer(dataType: str):
     match dataType:
         case "str_ptr":
             return utils.from_ptr_str
-        case "s16":
-            return utils.from_16
+        case "bool":
+            return utils.from_bool
         case "s8":
             return utils.from_s8
+        case "u8":
+            return utils.from_u8
+        case "s16":
+            return utils.from_16
+        case "u16":
+            return utils.from_16
         case "u32":
             return utils.from_u32
         case _:
@@ -39,6 +45,8 @@ def get_parser_and_size(dataType: str):
             return (utils.to_u8, 1)
         case "s16":
             return (utils.to_s16, 2)
+        case "u16":
+            return (utils.to_u16, 2)
         case "u32":
             return (utils.to_u32, 4)
         case _:
@@ -54,10 +62,6 @@ def serialize_asset(content: str, asset_config:str) -> bytearray:
         for entry, entryType in config.items():
             serializer = get_serializer(entryType)
             serialized_data += serializer(item[entry])
-    expected_data_size = item_count * item_size
-    print(len(serialized_data))
-    print(expected_data_size)
-    assert len(serialized_data) == expected_data_size
 
     return serialized_data
 
@@ -84,18 +88,21 @@ class PSXSegAssets(N64Segment):
             return rom[
                 utils.to_u32(src_ptr_data) - (self.vram_start - self.rom_start) :
             ]
+        config_file_name = f"tools/splat_ext/{self.name}_config.json"
+        with open(config_file_name,"r") as config_in:
+            config_json = config_in.read()
+        config = json.loads(config_json)
 
+        item_size = sum(get_parser_and_size(x)[1] for x in config.values())
         count = int(len(data) / item_size)
         expected_data_size = count * item_size
+
         if len(data) != expected_data_size:
             log.write(
                 f"data for '{self.name}' is {expected_data_size - len(data)} too long. Data might look incorrect.",
                 status="warn",
             )
-        config_file_name = f"tools/splat_ext/{self.name}_config.json"
-        with open(config_file_name,"r") as config_in:
-            config_json = config_in.read()
-        config = json.loads(config_json)
+    
         items = []
         for i in range(0, count):
             item_data = data[i * item_size :][:item_size]
@@ -123,7 +130,6 @@ class PSXSegAssets(N64Segment):
 if __name__ == "__main__":
     input_file_name = sys.argv[1]
     output_file_name = sys.argv[2]
-    print(f"BOOYAH {input_file_name} WITH {output_file_name}")
     config_file_name = input_file_name.replace(".json","_config.json")
     config_file_name = config_file_name.replace("assets/dra","tools/splat_ext")
     with open(config_file_name,"r") as config_in:
